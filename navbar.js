26eaf8b4f9666b988b9fabf3fe6d255cc7b001af
Jingshuai-cloud <xujingshuai@gmail.com>
2020-04-24 00:32:37 +1000

    add resume page

--- navbar.js
+++ navbar.js
@@ -4,3 +4,14 @@ $(".navTrigger").click(function () {
   $("#mainListDiv").toggleClass("show_list");
   $("#mainListDiv").fadeIn();
 });
+
+//-- Function used to shrink nav bar removing paddings and adding black background -->
+
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 50) {
+    $(".nav").addClass("affix");
+    console.log("OK");
+  } else {
+    $(".nav").removeClass("affix");
+  }
+});
